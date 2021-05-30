3660876bc0b624fa5619df6ed91c0e5fe502ef64
beginerbeginer <miyabi6125@gmail.com>
2021-05-30 10:47:43 +0900

    埼玉、福岡、熊本、群馬、石川、宮城だけ色を変更する

--- src/components/RenderLayers.js
+++ src/components/RenderLayers.js
@@ -7,6 +7,43 @@ export function renderLayers(props) {
 
 	if (!cityData) return [];
 
+
+	const jsonStr = `
+		{
+			"status": "200 OK",
+			"data": [
+				{"jcode": "04100", "rate": 0.0, "colorFlg": 0},
+				{"jcode": "11100", "rate": 0.2, "colorFlg": 1},
+				{"jcode": "10525", "rate": 0.4, "colorFlg": 2},
+				{"jcode": "43100", "rate": 0.6, "colorFlg": 3},
+				{"jcode": "40100", "rate": 0.8, "colorFlg": 4},
+				{"jcode": "33100", "rate": 1.0, "colorFlg": 5}
+			]
+		}
+	`
+
+	const rateData = JSON.parse(jsonStr)
+	const getColorRange = (geoJson, colorRange) => {
+
+		//　jsonStrのjcodeとpref.geojsonのJCODEが同じ値の場合matchesに値が入る
+		const matches = rateData.data.find(d => d.jcode === geoJson.properties['JCODE'])
+		console.log(matches); // 例{jcode: "33100", rate: 1, colorFlg: 5}
+		console.log('↑matches終了');
+
+		const idx = ((matches) => {
+			if (matches) { //matchesに値がある場合
+				return matches.colorFlg
+			} else {       // undefinedの場合
+				return 0
+			}
+		})(matches)
+		console.log(idx);
+		console.log('↑idx終了');
+		console.log(colorRange[idx]);
+		console.log('↑colorRange[idx]終了');
+		return colorRange[idx]
+	}
+
 	const city = [
 		new GeoJsonLayer({
 			id: 'city-layer',
@@ -16,7 +53,8 @@ export function renderLayers(props) {
 			filled: true,
 			lineWidthScale: 20,
 			lineWidthMinPixels: 0.5,
-			getFillColor: (d) => colorRange[d.properties['answer']],
+			//getFillColor: (d) => colorRange[d.properties['answer']],
+			getFillColor: (d) => getColorRange(d, colorRange),
 			getLineColor: [ 120, 120, 120 ],
 			onClick: onClick
 		})
